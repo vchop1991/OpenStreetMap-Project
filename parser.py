902161cc3276a8af14feb638ff227b8d0e127a1a
vchop1991 <36579668+vchop1991@users.noreply.github.com>
2018-02-18 11:03:57 +0800

    Add files via upload

---
+++ parser.py
@@ -0,0 +1,19 @@
+"""
+Parse the OSM file and count the number of unique tags
+"""
+
+import xml.etree.cElementTree as ET
+import pprint
+
+OSMFILE = "/Users/vinutachopra/Desktop/bengaluru_india.osm"
+
+def count_tags(filename):
+    tags = {}
+    for event, elem in ET.iterparse(filename):
+        if elem.tag in tags: 
+            tags[elem.tag] += 1
+        else:
+            tags[elem.tag] = 1
+    return tags
+
+pprint.pprint(count_tags(OSMFILE))
